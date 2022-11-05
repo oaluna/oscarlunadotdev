55f8f4c88f7ba3413d9688676fa7f0dd97558063
oscarluna <oscarluna.webdev@gmail.com>
2022-11-04 17:06:22 -0700

    part 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
         muted="true"
         loop="true"
         speed="0.25"
-        className="absolute w-full h-full absolute bottom-0 top-0 md:top-[-25vh] \ left-0 w-[100vw] h-[100vh] transform origin-[0.08% 100%] skew-y-[-16deg] scale-[120%] overflow-hidden border-none mix-blend-color-dodge z-[-1]"
+        className="absolute w-full h-full absolute bottom-0 top-0 md:top-[-25vh] \ left-0 w-[100vw] h-[100vh] transform origin-[0.08% 100%] skew-y-[-16deg] overflow-hidden border-none mix-blend-color-dodge z-[-1] scale-3 md:scale-1"
       >
         <source
           src="https://player.vimeo.com/external/368730818.sd.mp4?s=c3953421878536e1a7db904cae7dae03e7bfad19&amp;profile_id=164&amp;oauth2_token_id=57447761"
