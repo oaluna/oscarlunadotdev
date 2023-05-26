9cc1daf865a8ee1bbde905c9c1dbfebf023efbe0
oscarluna <oscarluna.webdev@gmail.com>
2023-05-26 14:13:00 -0700

    Rebase to 950f462331

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,6 +3,7 @@ import AltLogo from "./AltLogo";
 import { EXPERIENCE_DATA } from "../data/experienceData";
 import Fade from "react-reveal/Fade";
 
+<<<<<<< HEAD
 const Experience = () => {
   // 	const targets = document.querySelectorAll(".js-show-on-scroll");
 
@@ -33,6 +34,57 @@ const Experience = () => {
                   </h1>
                 </div>
               </Fade>
+=======
+const Experience = () => (
+  <section className="section snap-start lg:my-[32vh]" id="experience">
+    <div className="my-32 lg:flex lg:flex-row">
+      <div className="grid grid-cols-4">
+			<Fade right>
+        <div className="col-span-4 md:col-span-8 lg:col-span-12 leading-none lg:ml-5 lg:text-right">
+          <h1 className="lg:ml-[45vw] text-[10vh] lg:text-[20vh] leading-none uppercase text-slate-900">
+            Work Experience
+          </h1>
+          <h1 className="uppercase main_title leading-none text-[5vh] lg:text-[20vh] font-medium text-[#bb2649] pb-8">
+            My Most Recent Jobs
+          </h1>
+        </div>
+				</Fade>
+      </div>
+
+      <div className="flex flex-col items-start justify-evenly w-full h-auto lg:absolute lg:left-[-10vw]">
+			<Fade left>
+        {EXPERIENCE_DATA.map((job) => (
+          <div className="lg:w-[55vw] lg:ml-44 my-[8vh] lg:mt-0">
+            <div
+              className="overflow-hidden text-center rounded  bg-zinc-200 shadow-sm shadow-slate-900/50
+            h-full md:scale-[.6]  lg:scale-50 hover:scale-75 my-[-10vh] hover:transition-all hover:ease-in-out z-[0] hover:z-20"
+            >
+              <figure className="p-6 pb-0">
+                {job.logo ? (
+                  <>
+                    <img
+                      src={job.logo}
+                      alt="brand"
+                      className="h-24 w-24 mx-auto"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <AltLogo />
+                  </>
+                )}
+              </figure>
+
+              <div className="p-6">
+                <h3 className="mb-4 text-xl font-medium text-slate-900 bg-blend-color-burn space-x-2">
+                  {job.role}{" "}
+                </h3>
+                <p className="font-light text-slate-900 my-2">{job.time}</p>
+                <p className="text-slate-900 bg-blend-color-burn overflow-ellipsis">
+                  {job.description}
+                </p>
+              </div>
+>>>>>>> parent of 960f462 (Debugging carousel)
             </div>
           </Fade>
         </div>
@@ -76,8 +128,14 @@ const Experience = () => {
           </Fade>
         </div>
       </div>
+<<<<<<< HEAD
     </section>
   );
 };
+=======
+    </div>
+  </section>
+);
+>>>>>>> parent of 960f462 (Debugging carousel)
 
 export default Experience;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import Fade from "react-reveal/Fade";
 function Projects() {
   return (
     <>
+<<<<<<< HEAD
       <section
         className="section transition sub-section  snap-start relative w-screen h-auto lg:flex lg:flex-row lg:items-center lg:my-36 lg:h-[100vh]"
         id="projects"
@@ -21,6 +22,21 @@ function Projects() {
                 </p>
               </div>
             </Fade>
+=======
+      <section className="section snap-start relative w-screen h-full" id="projects">
+        <div className="container w-full md:mt-0 lg:mt-[-40vh] lg:py-36 px-0 mx-auto">
+          <div className="flex flex-row items-center md:text-left lg:float-right relative lg:absolute lg:top-56 lg:left-40 md:px-0 px-16 z-20 leading-none">
+					<Fade right delay={500}>
+            <div className="col-span-4 md:col-span-8 lg:col-span-12 my-12 lg:my-0">
+              <h1 className="uppercase text-[#bb2649] text-[10vh] lg:text-[20vh] leading-none">
+                Recent Projects
+              </h1>
+              <p className="main_title font-thin text-slate-900 leading-8 pb-12 text-[16px]">
+                Some of the more recent things I've worked on.
+              </p>
+            </div>
+						</Fade>
+>>>>>>> parent of 960f462 (Debugging carousel)
           </div>
           <Fade left>
             <CarouselComponent />
